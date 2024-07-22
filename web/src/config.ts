f0f910e8f96cfca6d8e234e9b35c52841392af84
joshkaye88 <jkaye@shaw.ca>
2024-07-21 21:36:07 -0700

    add dev/prod to env vars

--- web/src/config.ts
+++ web/src/config.ts
@@ -1,2 +1,5 @@
 
-export const API_URL = 'http://192.168.1.70:8000/api'
+// export const API_URL = 'http://192.168.1.70:8000/api'
+export const API_URL = import.meta.env.DEV
+    ? 'http://192.168.1.70:8000/api'
+    : 'https://bookshelf-app-backend.fly.dev'
