7b9cab38aa1676e5169f124a59a4fce7f6e08461
joshkaye88 <jkaye@shaw.ca>
2024-08-08 00:50:55 -0700

    add more links to footer, improve UI

--- web/src/layouts/components/Footer.tsx
+++ web/src/layouts/components/Footer.tsx
@@ -1,16 +1,16 @@
 import { Link } from "react-router-dom";
 import Logo from "./Logo";
 import Wrapper from "./Wrapper";
-import Divider from "../../components/common/Divider";
+import { useStore } from "../../store/store";
 
 export default function Footer() {
 
+    const { auth } = useStore()
+
     return (
-        <div className="relative pb-12">
+        <footer className="relative -z-0 pb-12 bg-primary-light/20">
             <Wrapper>
                 <div className="flex flex-col items-center gap-6">
-
-                    <Divider />
 
                     <button 
                         onClick={ () => window.scrollTo({ top: 0, behavior: 'smooth' })}
@@ -22,11 +22,28 @@ export default function Footer() {
                     <div className="flex gap-6">
                         <Link to="/">Home</Link>
                         <Link to="/about">About</Link>
+                        { auth.isAuth && (
+                            <Link to="/dashboard">Dashboard</Link>
+                        )}
+                        { !auth.isAuth && (
+                            <>
+                                <Link to="/login">Log In</Link>
+                                <Link to="/register">Register</Link>
+                            </>
+                        )}
                     </div>
 
+                    <p className="flex items-center justify-center flex-wrap gap-2">
+                        <span className="w-full text-center">Project developed by Josh Kaye</span>
+                        <a href="https://joshkaye.dev" target="_blank">Portfolio</a>
+                        <span className="w-1 h-1 rounded-full bg-accent" />
+                        <a href="https://github.com/kaye360" target="_blank">GitHub</a>
+                    </p>
+
                     <Logo />
+
                 </div>
             </Wrapper>
-        </div>
+        </footer>
     )
 }
