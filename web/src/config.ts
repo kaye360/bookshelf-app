5d1b2f7c04fe0b4bfb2ddcfa962951d1d4d51ef7
joshkaye88 <jkaye@shaw.ca>
2024-08-09 09:16:19 -0700

    update API_URL to new vapor project without /

--- web/src/config.ts
+++ web/src/config.ts
@@ -3,6 +3,6 @@ export const APP_NAME = 'Bookshelf.app'
 
 export const API_URL = import.meta.env.DEV
     ? 'http://192.168.1.70:8000/api'
-    : 'https://ptbjwwdu53mftqio4jk56uno7m0uxrda.lambda-url.us-west-2.on.aws/'
+    : 'https://ptbjwwdu53mftqio4jk56uno7m0uxrda.lambda-url.us-west-2.on.aws'
 
 export const HTML_TITLE = 'Bookshelf.app - Track your book collection'
